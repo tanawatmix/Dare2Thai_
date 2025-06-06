a9ad95850b760b3b633e2fef46e469a04aa9d443
tanawatmix <s6552410002@sau.ac.th>
2025-06-06 16:57:13 +0700

    V1.1

--- src/views/components/Card.tsx
+++ src/views/components/Card.tsx
@@ -9,7 +9,7 @@ const Card: React.FC<CardProps> = ({ image, title, description }) => {
   return (
     <div className="py-20 px-5">
       <div className=" max-w-5xl w-full border rounded-lg p-5 hover:shadow-lg transition-shadow duration-300 object-center mx-auto bg-white ">
-        <img src={image} alt={title} className="object-fit w-full h-80 " />
+        <img src={image} alt={title} className="object-contain w-full h-80 " />
         <div className="mt-4">
           <h1 className="text-3xl font-extrabold bg-gradient-to-r from-pink-500 via-pink-400 to-orange-300 bg-clip-text text-transparent text-center">{title}</h1>
           <p className="text-gray-600 mt-2 text-left">{description}</p>
